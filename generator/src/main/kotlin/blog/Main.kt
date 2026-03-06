d9a3abaf717734bb51b715702064dc59cc6f8fa9
Takuma Homma <nagomimatcha@gmail.com>
2026-03-06 22:53:53 +0900

    Generate feed.xml

--- generator/src/main/kotlin/blog/Main.kt
+++ generator/src/main/kotlin/blog/Main.kt
@@ -1,6 +1,10 @@
 package blog
 
 fun main() {
+    val isDev = System.getenv("DEV") == "1"
     Generator().run()
     IndexPageGenerator.generate()
+    if (!isDev) {
+        FeedGenerator.main(emptyArray())
+    }
 }
